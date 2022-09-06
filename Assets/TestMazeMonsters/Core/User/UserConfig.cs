817c9f9201b44cd93c131a84e8bdd18e5545d5a7
DrTressi <DrTressi@yandex.ru>
2022-09-06 16:16:37 +0700

    feat(gp): user config added

---
+++ Assets/TestMazeMonsters/Core/User/UserConfig.cs
@@ -0,0 +1,80 @@
+using System.IO;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using UnityEngine;
+using Zenject;
+
+namespace TestMazeMonsters.Core.User
+{
+    public class UserConfig : IInitializable
+    {
+        private string CONFIG_FILE_NAME = "userconfig.json";
+        private string DEFAULT_CONFIG_DIR_NAME = "DefaultConfigs";
+
+        private UserData _userData;
+        private string _persistentPath;
+
+        private string PersistentPath
+        {
+            get
+            {
+                if (string.IsNullOrEmpty(_persistentPath))
+                    _persistentPath = Path.Combine(Application.persistentDataPath, CONFIG_FILE_NAME);
+                return _persistentPath;
+            }
+        }
+
+        public UserData UserData
+        {
+            get
+            {
+                if (_userData == null)
+                    Load();
+                return _userData;
+            }
+        }
+
+        public void Initialize()
+        {
+            JsonConvert.DefaultSettings = () =>
+            {
+                var settings = new JsonSerializerSettings();
+                settings.Converters.Add(new StringEnumConverter { CamelCaseText = true });
+                return settings;
+            };
+            
+            Load();
+        }
+
+        private void Save()
+        {
+            File.WriteAllText(PersistentPath,JsonConvert.SerializeObject(_userData));
+        }
+
+        private void Load()
+        {
+            if (_userData != null)
+            {
+                return;
+            }
+            if (File.Exists(PersistentPath))
+            {
+                _userData = JsonConvert.DeserializeObject<UserData>(File.ReadAllText(PersistentPath));
+                Debug.Log("Load"+JsonConvert.SerializeObject(_userData));
+            }
+            else
+            {
+                LoadDefault();
+            }
+        }
+
+        private void LoadDefault()
+        {
+            string defaultPath =
+                Path.Combine(DEFAULT_CONFIG_DIR_NAME, Path.GetFileNameWithoutExtension(CONFIG_FILE_NAME));
+            _userData = JsonConvert.DeserializeObject<UserData>(Resources.Load<TextAsset>(defaultPath).text);
+            Save();
+            Debug.Log("LoadDefault"+JsonConvert.SerializeObject(_userData));
+        }
+    }
+}
---
+++ Assets/TestMazeMonsters/Core/User/UserData.cs
@@ -0,0 +1,15 @@
+using System.Collections.Generic;
+using TestMazeMonsters.Core.Input.Enums;
+using UnityEngine;
+
+namespace TestMazeMonsters.Core.User
+{
+    ///[JsonConverter(typeof(StringEnumConverter))]
+    public class UserData
+    {
+        //Todo Вместо KeyCode должен быть List<KeyCode>, чтобы назначать несколько клавишь на одно дейсвие
+        private Dictionary<InputCmdId, KeyCode> _controls = new Dictionary<InputCmdId, KeyCode>();
+
+        public Dictionary<InputCmdId, KeyCode> Controls => _controls;
+    }
+}
